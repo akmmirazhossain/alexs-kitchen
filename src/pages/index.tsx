20ce2bcbf8bf6d5e9aa43d8782b1637f41568333
AKM Miraz Hossain <akmmirazhossain@gmail.com>
2024-06-09 06:06:16 +0600

    images added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -154,190 +154,213 @@ const MenuPage = () => {
   };
 
   return (
-    <Container>
-      <Typography variant="h4" gutterBottom>
-        Menu
-      </Typography>
-      <Box display="flex" justifyContent="space-between" mb={2}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleAddItemClick}
-        >
-          Add Item
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={restoreDataFromAPI}
-        >
-          Restore Data from API
-        </Button>
-      </Box>
+    <div
+      style={{
+        backgroundImage: `url('food_bg.webp')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+        // Add other styles as needed
+      }}
+    >
+      <Container>
+        <div className="text-5xl py-10 border-b-2">Alex's Kitchen</div>
+        <div className="flex justify-between pt-4">
+          <Typography variant="h4" gutterBottom>
+            Our Menu
+          </Typography>
+          <Box display="flex" justifyContent="space-between" mb={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleAddItemClick}
+            >
+              Add Item
+            </Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={restoreDataFromAPI}
+            >
+              Restore Data from API
+            </Button>
+          </Box>
+        </div>
 
-      <Dialog open={open && isAddingNewItem} onClose={handleAddItemCancel}>
-        <DialogTitle>Add New Menu Item</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            name="name"
-            label="Name"
-            fullWidth
-            onChange={handleInputChange}
-          />
-          <TextField
-            margin="dense"
-            name="category"
-            label="Category"
-            fullWidth
-            onChange={handleInputChange}
-          />
-          <TextField
-            margin="dense"
-            name="price"
-            label="Price"
-            type="number"
-            fullWidth
-            onChange={handleInputChange}
-          />
-          <TextField
-            margin="dense"
-            name="details"
-            label="Details"
-            fullWidth
-            onChange={handleInputChange}
-          />
-          <TextField
-            margin="dense"
-            name="image"
-            label="Image URL"
-            fullWidth
-            onChange={handleInputChange}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleAddItemCancel} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleSaveNewItem} color="primary">
-            Add Item
-          </Button>
-        </DialogActions>
-      </Dialog>
+        <Dialog open={open && isAddingNewItem} onClose={handleAddItemCancel}>
+          <DialogTitle>Add New Menu Item</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              name="name"
+              label="Name"
+              fullWidth
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="category"
+              label="Category"
+              fullWidth
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="price"
+              label="Price"
+              type="number"
+              fullWidth
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="details"
+              label="Details"
+              fullWidth
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              name="image"
+              label="Image URL"
+              fullWidth
+              onChange={handleInputChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleAddItemCancel} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleSaveNewItem} color="primary">
+              Add Item
+            </Button>
+          </DialogActions>
+        </Dialog>
 
-      <Dialog open={open && !isAddingNewItem} onClose={handleDialogClose}>
-        <DialogTitle>Edit Menu Item</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            name="name"
-            label="Name"
-            fullWidth
-            value={newItem.name}
-            onChange={handleInputChange}
-          />
+        <Dialog open={open && !isAddingNewItem} onClose={handleDialogClose}>
+          <DialogTitle>Edit Menu Item</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              name="name"
+              label="Name"
+              fullWidth
+              value={newItem.name}
+              onChange={handleInputChange}
+            />
 
-          <TextField
-            margin="dense"
-            name="category"
-            label="Category"
-            fullWidth
-            value={newItem.category}
-            onChange={handleInputChange}
-          />
+            <TextField
+              margin="dense"
+              name="category"
+              label="Category"
+              fullWidth
+              value={newItem.category}
+              onChange={handleInputChange}
+            />
 
-          <TextField
-            margin="dense"
-            name="price"
-            label="Price"
-            type="number"
-            fullWidth
-            value={newItem.price}
-            onChange={handleInputChange}
-          />
+            <TextField
+              margin="dense"
+              name="price"
+              label="Price"
+              type="number"
+              fullWidth
+              value={newItem.price}
+              onChange={handleInputChange}
+            />
 
-          <TextField
-            margin="dense"
-            name="details"
-            label="Details"
-            fullWidth
-            value={newItem.details}
-            onChange={handleInputChange}
-          />
+            <TextField
+              margin="dense"
+              name="details"
+              label="Details"
+              fullWidth
+              value={newItem.details}
+              onChange={handleInputChange}
+            />
 
-          <TextField
-            margin="dense"
-            name="image"
-            label="Image URL"
-            fullWidth
-            value={newItem.image}
-            onChange={handleInputChange}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleDialogClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleSaveEdit} color="primary">
-            Save
-          </Button>
-        </DialogActions>
-      </Dialog>
+            <TextField
+              margin="dense"
+              name="image"
+              label="Image URL"
+              fullWidth
+              value={newItem.image}
+              onChange={handleInputChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleDialogClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={handleSaveEdit} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
 
-      <Grid container spacing={3}>
-        {menuItems
-          .slice()
-          .reverse()
-          .map((item) => (
-            <Grid item key={item.id} xs={12} sm={6} md={4} className="relative">
-              <Box boxShadow={4} borderRadius={8} overflow="hidden">
-                <Card>
-                  <CardContent className="grid grid-cols-2">
-                    <div>
-                      <img
-                        src={item.image}
-                        alt={item.name}
-                        style={{ width: "100%", marginTop: "10px" }}
-                      />
-                    </div>
-                    <div>
-                      <Typography variant="h6" component="h2">
-                        {item.name}
-                      </Typography>
-                      <Typography color="textSecondary" gutterBottom>
-                        Category: {item.category}
-                      </Typography>
-                      <Typography color="textSecondary" gutterBottom>
-                        Price: {item.price} TK
-                      </Typography>
-                      <Typography variant="body2" component="p">
-                        {item.details}
-                      </Typography>
-                    </div>
+        <Grid container spacing={3}>
+          {menuItems
+            .slice()
+            .reverse()
+            .map((item) => (
+              <Grid
+                item
+                key={item.id}
+                xs={12}
+                sm={6}
+                md={4}
+                className="relative"
+              >
+                <Box boxShadow={4} borderRadius={8} overflow="hidden">
+                  <Card>
+                    <CardContent className="grid grid-cols-2">
+                      <div>
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                          style={{ width: "100%", marginTop: "10px" }}
+                        />
+                      </div>
+                      <div>
+                        <Typography variant="h6" component="h2">
+                          {item.name}
+                        </Typography>
+                        <Typography color="textSecondary" gutterBottom>
+                          {item.category}
+                        </Typography>
+                        <Typography color="textSecondary" gutterBottom>
+                          Price: {item.price} TK
+                        </Typography>
+                        <Typography variant="body2" component="p">
+                          {item.details}
+                        </Typography>
+                      </div>
 
-                    <div className="absolute top-10 right-2 w-6">
-                      <IconButton onClick={(e) => handleMenuClick(e, item)}>
-                        <FontAwesomeIcon size="sm" icon={faEllipsisVertical} />
-                      </IconButton>
-                    </div>
+                      <div className="absolute top-10 right-2 w-6">
+                        <IconButton onClick={(e) => handleMenuClick(e, item)}>
+                          <FontAwesomeIcon
+                            size="sm"
+                            icon={faEllipsisVertical}
+                          />
+                        </IconButton>
+                      </div>
 
-                    <Menu
-                      anchorEl={anchorEl}
-                      open={Boolean(anchorEl)}
-                      onClose={handleClose}
-                    >
-                      <MenuItem onClick={handleEdit}>Edit</MenuItem>
-                      <MenuItem onClick={handleDelete}>Delete</MenuItem>
-                    </Menu>
-                  </CardContent>
-                </Card>
-              </Box>
-            </Grid>
-          ))}
-      </Grid>
-    </Container>
+                      <Menu
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                      >
+                        <MenuItem onClick={handleEdit}>Edit</MenuItem>
+                        <MenuItem onClick={handleDelete}>Delete</MenuItem>
+                      </Menu>
+                    </CardContent>
+                  </Card>
+                </Box>
+              </Grid>
+            ))}
+        </Grid>
+      </Container>
+    </div>
   );
 };
 
